8122d576290fb88e0b287a3d4f444d556231ca3e
Mariellis <139721905+Mariellis444@users.noreply.github.com>
2024-06-26 02:39:56 +0000

    pig

--- pig.py
+++ pig.py
@@ -3,7 +3,7 @@ import random
 def roll():
     min_value = 1
     max_value = 6
-    roll = random.randin(min_value, max_value)
+    roll = random.randint(min_value, max_value)
 
     return roll 
 
